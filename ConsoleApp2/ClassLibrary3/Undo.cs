385be43b14bc98ef0a47b32c0b800ddea5365737
SklyarLand <sushu1998@mail.ru>
2018-12-05 23:13:02 +0500

    Version 1.0

---
+++ ConsoleApp2/ClassLibrary3/Undo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commands
+{
+    public class UnDo : ICommand  //команда отмены действия
+    {
+
+        
+        public void Execute(List<ICommand> ts, List<int> Unit)
+        {
+            Console.WriteLine("Удален элемент: {0}", Unit[Unit.Count() - 1]);
+            ts.RemoveAt(ts.Count()-1);
+            Unit.RemoveAt(Unit.Count() - 1);
+        }
+        public override string ToString()
+        {
+            return "\tотменить действие";
+        }
+
+    }
+}
